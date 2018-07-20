fc6491c059203c0526433235759781315669190e
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2018-07-20 15:32:12 +0000

    Add registerDate to user model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const UserSchema = new mongoose.Schema({
     username:String,
     password:String,
+    registerDate: {type: Date, default: Date.now},
     todos: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Todo"
